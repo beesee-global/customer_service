48dd92d4036f1fecc9b019db9d6316940ed3dc6a
rodelagocoy17 <141696110+rodelagocoy17@users.noreply.github.com>
2024-04-01 18:06:58 +0800

    Add files via upload

---
+++ src/components/Form.tsx
@@ -0,0 +1,106 @@
+import Button from 'react-bootstrap/Button'
+import Form from 'react-bootstrap/Form';
+import Card from 'react-bootstrap/Card';
+import { DataModal } from '../models/Data';
+import { useState } from 'react';
+import { InputGroup } from 'react-bootstrap';
+// import { Form, Field } from 'react-final-form'
+
+interface StudentProps {
+  children?: React.ReactNode;
+}
+
+const MainForm: React.FC<StudentProps> = ({}) => {
+
+const initialFormState: DataModal = {
+    FirstName: '',
+    LastName: '',
+    Email: '',
+    Address: '',
+};
+
+const [formState, setFormState] = useState<DataModal>(initialFormState);
+
+  const handleOnSubmit = (e: any) => {
+    const formEle = document.querySelector("form")
+    e.preventDefault()
+    setFormState(initialFormState)
+    const formData = new FormData(formEle)
+    fetch("https://script.google.com/macros/s/AKfycbxjsE5dbDKkZTwNCiadf01RvQEG2cpgf0W5JU5f4R8QvDY1j3cBDlsO9PFiBN2W-mR_/exec", {
+      method: "POST",
+      body: formData
+    }).then((res) => res.json()).then((data) => {
+      console.log(data)
+      setFormState(initialFormState)
+    }).catch((error) => console.log(error))
+  }
+
+  const handleInputChange= (e: any) => {
+    setFormState(e)
+  }
+
+  return (
+    <Card>
+      <Card.Body>
+        <Form onSubmit={handleOnSubmit}>
+          <InputGroup className="mb-3">
+            <InputGroup.Text id="inputGroup-sizing-default">
+              First Name
+            </InputGroup.Text>
+            <Form.Control
+              aria-label="Default"
+              aria-describedby="inputGroup-sizing-default"
+              type="text"
+              name="FirstName"
+              value={formState.FirstName}
+              onChange={handleInputChange}
+            />
+          </InputGroup>
+          <InputGroup className="mb-3">
+            <InputGroup.Text id="inputGroup-sizing-default">
+              Last Name
+            </InputGroup.Text>
+            <Form.Control
+              aria-label="Default"
+              aria-describedby="inputGroup-sizing-default"
+              type="text"
+              name="LastName"
+              value={formState.LastName}
+              onChange={handleInputChange}
+            />
+          </InputGroup>
+          <InputGroup className="mb-3">
+            <InputGroup.Text id="inputGroup-sizing-default">
+              Email
+            </InputGroup.Text>
+            <Form.Control
+              aria-label="Default"
+              aria-describedby="inputGroup-sizing-default"
+              type="email"
+              name="Email"
+              value={formState.Email}
+              onChange={handleInputChange}
+            />
+          </InputGroup>
+          <InputGroup className="mb-3">
+            <InputGroup.Text id="inputGroup-sizing-default">
+              Address
+            </InputGroup.Text>
+            <Form.Control
+              aria-label="Default"
+              aria-describedby="inputGroup-sizing-default"
+              type="text"
+              name="Address"
+              value={formState.Address}
+              onChange={handleInputChange}
+            />
+          </InputGroup>
+          <Button type='submit' variant="primary me-2">Submit</Button>
+        </Form>
+      </Card.Body>
+    </Card>
+
+  )
+}
+
+export default MainForm;
---
+++ src/models/Data.tsx
@@ -0,0 +1,6 @@
+export interface DataModal {
+    FirstName: '',
+    LastName: '',
+    Email: '',
+    Address: ''
+}
